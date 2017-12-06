ac6c77238d5882b628d112bfd4eb7835b7874b2a
Eriko <erik.osterlund@bahnhof.se>
2017-12-06 16:01:22 +0100

    test

--- app/src/main/java/com/example/erik/questforglory/activities/Enchanting.java
+++ app/src/main/java/com/example/erik/questforglory/activities/Enchanting.java
@@ -1,15 +1,17 @@
 package com.example.erik.questforglory.activities;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.erik.questforglory.R;
+import com.example.erik.questforglory.classes.GearPiece;
 import com.example.erik.questforglory.classes.Monster;
 import com.example.erik.questforglory.classes.Player;
 import com.example.erik.questforglory.classes.Potion;
-import com.example.erik.questforglory.R;
+import com.example.erik.questforglory.classes.Skill;
 
 public class Enchanting extends AppCompatActivity {
 
@@ -19,12 +21,18 @@ public class Enchanting extends AppCompatActivity {
     Monster spriggan;
     Monster golem;
     Potion healthPotion;
+    Potion damagePotion;
+    Skill strike;
+    Skill charge;
+    Skill mend;
+    Skill defenseUp;
+    GearPiece gearPiece;
     TextView level;
     TextView XP;
     TextView gold;
     String levelText;
     String XPText;
-    String herbsText;
+    String soulDustText;
     String goldText;
 
     @Override
@@ -36,6 +44,11 @@ public class Enchanting extends AppCompatActivity {
         spriggan = (Monster) getIntent().getSerializableExtra("spriggan");
         golem = (Monster) getIntent().getSerializableExtra("golem");
         healthPotion = (Potion) getIntent().getSerializableExtra("healthPotion");
+        damagePotion = (Potion) getIntent().getSerializableExtra("damagePotion");
+        strike = (Skill) getIntent().getSerializableExtra("strike");
+        charge = (Skill) getIntent().getSerializableExtra("charge");
+        mend = (Skill) getIntent().getSerializableExtra("mend");
+        defenseUp = (Skill) getIntent().getSerializableExtra("defenseUp");
 
         level = findViewById(R.id.level);
         XP = findViewById(R.id.XP);
@@ -43,7 +56,7 @@ public class Enchanting extends AppCompatActivity {
 
         levelText = "Level " + (int) player.getLevel();
         XPText = "XP " + (int) player.getXP() + " / " + (int) player.getXPToNextLevel();
-        herbsText = " Herbs: " + (int) player.getHerbs();
+        soulDustText = " Ores: " + (int) player.getOres();
         goldText = " Gold: " + (int) player.getGold();
 
         level.setText(levelText);
@@ -56,6 +69,12 @@ public class Enchanting extends AppCompatActivity {
         bundle.putSerializable("spriggan", spriggan);
         bundle.putSerializable("golem", golem);
         bundle.putSerializable("healthPotion", healthPotion);
+        bundle.putSerializable("damagePotion", damagePotion);
+        bundle.putSerializable("strike", strike);
+        bundle.putSerializable("charge", charge);
+        bundle.putSerializable("mend", mend);
+        bundle.putSerializable("defenseUp", defenseUp);
+        bundle.putSerializable("gearPiece", gearPiece);
         intent.putExtras(bundle);
         startActivity(intent);
     }
@@ -77,10 +96,6 @@ public class Enchanting extends AppCompatActivity {
     }
     public void gear(View view) {
         intent = new Intent(this, Gear.class);
-        saveObjectsInBundle();
-    }
-    public void kingdom(View view) {
-        intent = new Intent(this, Kingdom.class);
         saveObjectsInBundle();
     }
     public void quests(View view) {
